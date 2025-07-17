cdfd53c01144ddc88bb22861e7ced2c885aabe9d
sbraatz <simonebraatz97@gmail.com>
2025-07-17 15:32:19 -0300

    aula 4 - exercicios

---
+++ Conceitos Basicos/dadosUsuario.js
@@ -0,0 +1,11 @@
+const nome = "Luiz Otavio Miranda";
+const sobrenome = "Miranda";
+const idade = 30;
+const peso = 84;
+const altura = 1.84;
+let anoAtual = 2010;
+
+const imc = peso / (altura * altura);
+const anoNascimento = anoAtual - idade;
+
+console.log
---
+++ Conceitos Basicos/script.js
@@ -0,0 +1,23 @@
+let idade = 27;
+let nome = "Simone";
+let ativo = true;
+let nota;
+let cidade = null;
+
+console.log(idade)
+console.log(nome)
+console.log(ativo)
+console.log(nota)
+console.log(cidade)
+
+nome = 'maria'
+var Nome = 'maria'
+
+let nomeCliente = 'joao'
+nomecliente = 'joao'
+
+console.log(nome)
+
+console.log(nome, nomeCliente, nomecliente)
+
+
