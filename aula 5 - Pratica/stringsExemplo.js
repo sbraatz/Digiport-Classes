10cc76cf981250b87ddef2d1283c55614e6005ff
sbraatz <simonebraatz97@gmail.com>
2025-07-21 15:38:09 -0300

    aula 5 - pratica

---
+++ aula 5 - Pratica/stringsExemplo.js
@@ -0,0 +1,10 @@
+//               012345
+let umaString = "Javascript é incrível"
+//console.log(umaString[5])
+//umaString.charAt(5)
+//console.log(umaString.replace("incrível", "poderoso"))
+//console.log(umaString.length)
+
+
+
+ 
