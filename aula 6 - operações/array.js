8b06661240d61d0f70b5893a2a63a77eafe2c760
sbraatz <simonebraatz97@gmail.com>
2025-07-24 15:31:18 -0300

    aula 6 - boolean

---
+++ aula 6 - operações/array.js
@@ -0,0 +1,4 @@
+let a = [123]
+let b = "5" + 5
+
+// console.log(parseInt(b) typeof parseInt(b)))
---
+++ aula 6 - operações/boolean.js
@@ -0,0 +1,22 @@
+/*
+5 > 3
+10 == 5
+NÃO (5 < 10)
+6 % 2 == 0 E 6 > 3
+*/
+
+let a = 5
+let b = 3
+let c = 10
+let d = 6
+
+console.log(a > b);
+console.log(c == a);
+console.log(!(a < 10));
+console.log((6 % 2 == 0 && 6 >3));
+
+console.log(b < a);
+
+
+
+
