fa39849492db0d02341985c0bb76e15c33314e0a
sbraatz <simonebraatz97@gmail.com>
2025-08-07 14:51:35 -0300

    aula 10 - estrutura de repeticao loop

---
+++ aula 10 - estrutura de repeticao loop/exercicio1.js
@@ -0,0 +1,6 @@
+const nomes = ["Ana", "Bruna", "Eva"];
+const sabores = ["chocolate", "morango", "pistache"];
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log("Nome: " + nomes[i] + "   |   Sabor favorito: " + sabores[i]);
+}
