cfc7787f914dc0565ac62cc09f76270ec9024350
sbraatz <simonebraatz97@gmail.com>
2025-08-25 14:56:17 -0300

    aula 15

---
+++ aula 15/exercicio2.js
@@ -0,0 +1,18 @@
+let listaDeTarefas = []
+
+function adicionarTarefa(item) {
+    listaDeTarefas.push(item)
+
+}
+
+adicionarTarefa("estudar fundamentos de programação")
+adicionarTarefa("entrega da turma)
+    
+
+
+
+console.log(listaDeTarefas)
+
+
+
+
