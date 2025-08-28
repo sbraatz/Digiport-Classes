8f8f358665f77f11c62fa001ffc73ffe941d384c
sbraatz <simonebraatz97@gmail.com>
2025-08-28 15:10:33 -0300

    aula 16 - paradigmas

---
+++ aula 16 - paradigmas/aula.js
@@ -0,0 +1,9 @@
+/* Paradigmas de programação
+
+Modelos ou estilos que definem a forma como se escreve a solução de um problema.
+Descrevem como representar e estruturar a solução computacional
+Sua importancia está em ajudar a construir códigos mais claros, organizados e legíveis.
+
+
+
+*/
